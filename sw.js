1c490dc08361f1634dfee87cfb89b66b7ecc962c
irvinegraeme4-ops <irvinegraeme4@gmail.com>
2026-08-17 02:29:46 +0100

    Add offline support

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='workout-book-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
